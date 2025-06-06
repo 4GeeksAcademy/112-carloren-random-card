4774600b6e65924b2ef3c4f16b9e06e64fd0af00
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-06 11:39:34 +0000

    palo inferior invertido e indicativo 6 y 9

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ function cardGen() {
 
   let cardNum = cardNums[Math.floor(Math.random() * cardNums.length)]
 
+  let cardValue = (cardNum === 6 || cardNum === 9) ? `<p class="align-self-center text-center text-decoration-underline" style="font-size: 15em">${cardNum}</p>` : `<p class="align-self-center text-center" style="font-size: 15em">${cardNum}</p>`
+
   let spades = `
   <div id="card" class="card m-5 align-self-center" style="width: 450px">
     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
@@ -13,8 +15,8 @@ function cardGen() {
       <path
         d="M7.184 11.246A3.5 3.5 0 0 1 1 9c0-1.602 1.14-2.633 2.66-4.008C4.986 3.792 6.602 2.33 8 0c1.398 2.33 3.014 3.792 4.34 4.992C13.86 6.367 15 7.398 15 9a3.5 3.5 0 0 1-6.184 2.246 20 20 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a20 20 0 0 0 1.582-2.907" />
     </svg>
-    <p class="align-self-center text-center" style="font-size: 15em">${cardNum}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+    ${cardValue}
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" style="transform: scaleY(-1)"
       class="bi bi-suit-spade-fill m-3 align-self-end" viewBox="0 0 16 16">
       <path
         d="M7.184 11.246A3.5 3.5 0 0 1 1 9c0-1.602 1.14-2.633 2.66-4.008C4.986 3.792 6.602 2.33 8 0c1.398 2.33 3.014 3.792 4.34 4.992C13.86 6.367 15 7.398 15 9a3.5 3.5 0 0 1-6.184 2.246 20 20 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a20 20 0 0 0 1.582-2.907" />
@@ -24,13 +26,13 @@ function cardGen() {
 
   let hearts = `
   <div id="card" class="card m-5 align-self-center" style="width: 450px">
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" 
       class="bi bi-suit-heart-fill m-3 text-danger" viewBox="0 0 16 16">
       <path
         d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
     </svg>
-    <p class="align-self-center text-center" style="font-size: 15em">${cardNum}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+    ${cardValue}
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" style="transform: scaleY(-1)"
       class="bi bi-suit-heart-fill m-3 align-self-end text-danger" viewBox="0 0 16 16">
       <path
         d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
@@ -45,8 +47,8 @@ function cardGen() {
       <path
         d="M11.5 12.5a3.5 3.5 0 0 1-2.684-1.254 20 20 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a20 20 0 0 0 1.582-2.907 3.5 3.5 0 1 1-2.538-5.743 3.5 3.5 0 1 1 6.708 0A3.5 3.5 0 1 1 11.5 12.5" />
     </svg>
-    <p class="align-self-center text-center" style="font-size: 15em">${cardNum}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+    ${cardValue}
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" style="transform: scaleY(-1)"
       class="bi bi-suit-club-fill m-3 align-self-end" viewBox="0 0 16 16">
       <path
         d="M11.5 12.5a3.5 3.5 0 0 1-2.684-1.254 20 20 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a20 20 0 0 0 1.582-2.907 3.5 3.5 0 1 1-2.538-5.743 3.5 3.5 0 1 1 6.708 0A3.5 3.5 0 1 1 11.5 12.5" />
@@ -61,8 +63,8 @@ function cardGen() {
       <path
         d="M2.45 7.4 7.2 1.067a1 1 0 0 1 1.6 0L13.55 7.4a1 1 0 0 1 0 1.2L8.8 14.933a1 1 0 0 1-1.6 0L2.45 8.6a1 1 0 0 1 0-1.2" />
     </svg>
-    <p class="align-self-center text-center" style="font-size: 15em">${cardNum}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+    ${cardValue}
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" style="transform: scaleY(-1)"
       class="bi bi-suit-diamond-fill m-3 text-danger align-self-end" viewBox="0 0 16 16">
       <path
         d="M2.45 7.4 7.2 1.067a1 1 0 0 1 1.6 0L13.55 7.4a1 1 0 0 1 0 1.2L8.8 14.933a1 1 0 0 1-1.6 0L2.45 8.6a1 1 0 0 1 0-1.2" />
@@ -95,7 +97,6 @@ function clockCard() {
   oneSecond = setInterval(() => {
     countDown--
 
-    console.log(countDown);
     if (document.getElementById("countDown") === null) {
       document.body.innerHTML += `<p id="countDown" class="align-self-center" style="font-size: 50px">${countDown}</p>`
     } else {
